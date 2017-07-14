64f81a4f98b49a51bace116bfb05d61fc5182281
Administrator <Administrator@SC-201702132046>
2017-07-14 10:42:20 +0800

    1.0.3 业务接口

--- xiaojingit/src/main/java/org/j137/xiaojin/systemmag/service/impl/MenuService.java
+++ xiaojingit/src/main/java/org/j137/xiaojin/systemmag/service/impl/MenuService.java
@@ -1,12 +1,17 @@
 package org.j137.xiaojin.systemmag.service.impl;
 
+import org.j137.xiaojin.beans.Menu;
+import org.j137.xiaojin.beans.Page;
+import org.j137.xiaojin.systemmag.service.IMenuService;
+
+import com.sun.javafx.collections.MappingChange.Map;
 
 /**
  * @author Administrator
  * @version 1.0
  * @created 13-7ÔÂ-2017 11:39:39
  */
-public class MenuService {
+public class MenuService implements IMenuService{
 
 	public MenuService(){
 
@@ -14,6 +19,36 @@ public class MenuService {
 
 	public void finalize() throws Throwable {
 
+	}
+
+	@Override
+	public void saveMenu(Menu menu) {
+		
+		
+	}
+
+	@Override
+	public void updateMenu(Menu menu) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteMenu(Long id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Page findMenu(Page page) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Page findMenuByItem(Map map) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
