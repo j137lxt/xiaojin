588837959296b4f2cb4b0262086fba4b44037600
Administrator <Administrator@SC-201702132046>
2017-07-18 17:04:18 +0800

    1.2.0 系统管理持久层

--- xiaojingit/src/main/java/org/j137/xiaojin/beans/Organization.java
+++ xiaojingit/src/main/java/org/j137/xiaojin/beans/Organization.java
@@ -26,4 +26,74 @@ public class Organization {
 
 	}
 
+	public Integer getExist() {
+		return exist;
+	}
+
+	public void setExist(Integer exist) {
+		this.exist = exist;
+	}
+
+	public String getGrandOrg() {
+		return grandOrg;
+	}
+
+	public void setGrandOrg(String grandOrg) {
+		this.grandOrg = grandOrg;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getOrgCode() {
+		return orgCode;
+	}
+
+	public void setOrgCode(String orgCode) {
+		this.orgCode = orgCode;
+	}
+
+	public String getOrgName() {
+		return orgName;
+	}
+
+	public void setOrgName(String orgName) {
+		this.orgName = orgName;
+	}
+
+	public String getRemark() {
+		return remark;
+	}
+
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
+
+	public int getSort() {
+		return sort;
+	}
+
+	public void setSort(int sort) {
+		this.sort = sort;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	@Override
+	public String toString() {
+		return "Organization [exist=" + exist + ", grandOrg=" + grandOrg + ", id=" + id + ", orgCode=" + orgCode
+				+ ", orgName=" + orgName + ", remark=" + remark + ", sort=" + sort + ", state=" + state + "]";
+	}
+
 }
